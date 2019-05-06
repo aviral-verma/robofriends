684126dd0c8ba08319ab29f4ad8453e1b8dd6da6
Aviral Verma <aviralverma.verma@gmail.com>
2019-05-06 16:31:47 +0530

    Updates

---
+++ precache-manifest.5d66e475b649462a6fd533fbd2f1afed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e36f77b931dcf036c136fe88bd800d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "6db4def3a1e987745e22",
+    "url": "/static/css/main.b96c0cea.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "6db4def3a1e987745e22",
+    "url": "/static/js/main.16c34f6b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
